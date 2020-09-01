86bd6c14b86474eff3d768a886b3d6763cca2a27
Ceshine Lee <shuanck@gmail.com>
2020-09-01 13:28:44 +0800

    Detokenize the PAWS dataset

--- preprocessing/preprocess_paws.py
+++ preprocessing/preprocess_paws.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from nltk.tokenize.treebank import TreebankWordDetokenizer
 
 DATA_FILES = [
     "data/paws-swap/train.tsv",
@@ -16,6 +17,9 @@ if __name__ == "__main__":
         buffer.append(df[df.label == 1][["sentence1", "sentence2"]].copy())
         print(filepath, buffer[-1].shape[0])
     df_final = pd.concat(buffer, axis=0)
+    detokenizer = TreebankWordDetokenizer()
+    df_final["sentence1"] = df_final["sentence1"].apply(lambda x: detokenizer.detokenize(x.split(" ")))
+    df_final["sentence2"] = df_final["sentence2"].apply(lambda x: detokenizer.detokenize(x.split(" ")))
     del buffer
     train, test = train_test_split(df_final, test_size=0.2)
     valid, test = train_test_split(test, test_size=0.5)
