2f5dd758614ba8b861c788b0de83c1f4bb39653f
Ceshine Lee <shuanck@gmail.com>
2021-01-20 15:45:13 +0800

    Use sortish sampler

--- t2t/__init__.py
+++ t2t/__init__.py
@@ -5,6 +5,7 @@ from typing import Callable, Union
 
 import torch
 import torch.nn.functional as F
+from torch.utils.data.sampler import Sampler
 import numpy as np
 import pytorch_lightning as pl
 import pytorch_lightning_spells as pls
@@ -87,6 +88,56 @@ def collate_batch(batch, max_len, pad=0, decode_start_token=0, is_classifier=Fal
     )
 
 
+class SortSampler(Sampler):
+    def __init__(self, data_source, key):
+        self.data_source, self.key = data_source, key
+
+    def __len__(self):
+        return len(self.data_source)
+
+    def __iter__(self):
+        return iter(sorted(range(len(self.data_source)), key=self.key, reverse=True))
+
+
+class SortishSampler(Sampler):
+    """Returns an iterator that traverses the the data in randomly ordered batches that are approximately the same size.
+    The max key size batch is always returned in the first call because of pytorch cuda memory allocation sequencing.
+    Without that max key returned first multiple buffers may be allocated when the first created isn't large enough
+    to hold the next in the sequence.
+
+    Taken from fastai library.
+    """
+
+    def __init__(self, data_source, key, bs, chunk_size=50):
+        self.data_source, self.key, self.bs = data_source, key, bs
+        self.chunk_size = 50
+
+    def __len__(self): return len(self.data_source)
+
+    def __iter__(self):
+        while True:
+            idxs = np.random.permutation(len(self.data_source))
+            sz = self.bs * self.chunk_size
+            ck_idx = [idxs[i:i+sz] for i in range(0, len(idxs), sz)]
+            sort_idx = np.concatenate(
+                [sorted(s, key=self.key, reverse=True) for s in ck_idx])
+            sz = self.bs
+            ck_idx = [sort_idx[i:i+sz]for i in range(0, len(sort_idx), sz)]
+            # find the chunk with the largest key,
+            max_ck = np.argmax([self.key(ck[0]) for ck in ck_idx])
+            # then make sure it goes first.
+            if len(ck_idx[max_ck]) != self.bs:
+                continue
+            ck_idx[0], ck_idx[max_ck] = ck_idx[max_ck], ck_idx[0]
+            sort_idx = np.concatenate(np.random.permutation([
+                np.random.permutation(chunk.reshape(self.bs, -1)).reshape(-1)
+                for chunk in ck_idx[1:-1]
+            ]))
+            sort_idx = np.concatenate((ck_idx[0], sort_idx, ck_idx[-1]))
+            break
+        return iter(sort_idx)
+
+
 @dataclass
 class BaseConfig:
     base_t5_model: str
@@ -128,9 +179,14 @@ class T5BaseModel(pl.LightningModule):
         return self.model(**input_tensors)[0]
 
     def train_dataloader(self):
+        sampler = SortishSampler(
+            self.train_dataset,
+            key=lambda x: len(self.train_dataset[x][0]),
+            bs=self.config.batch_size
+        )
         return DataLoader(
-            self.train_dataset, num_workers=4 if self.config.tpu_cores == 0 else 1, shuffle=True, drop_last=True,
-            batch_size=self.config.batch_size, collate_fn=self.collate_fn)
+            self.train_dataset, num_workers=4 if self.config.tpu_cores == 0 else 1, shuffle=False, drop_last=True,
+            batch_size=self.config.batch_size, collate_fn=self.collate_fn, sampler=sampler)
 
     def get_progress_bar_dict(self):
         # don't show the experiment version number
