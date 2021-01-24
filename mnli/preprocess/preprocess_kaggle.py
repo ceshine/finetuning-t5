8b4058f8078e20aa7404ba8e0376c53cf166b452
Ceshine Lee <shuanck@gmail.com>
2021-01-24 12:52:46 +0800

    Reduce the validation size of the kaggle dataset

--- mnli/preprocess/preprocess_kaggle.py
+++ mnli/preprocess/preprocess_kaggle.py
@@ -3,6 +3,6 @@ from sklearn.model_selection import train_test_split
 
 if __name__ == "__main__":
     df_train = pd.read_csv("data/kaggle/train.csv")
-    train, valid = train_test_split(df_train, test_size=0.2)
+    train, valid = train_test_split(df_train, test_size=0.1)
     train.to_csv('data/kaggle/train_split.csv', index=False)
     valid.to_csv('data/kaggle/valid.csv', index=False)
